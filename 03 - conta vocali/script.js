55d2ac1925de345b27673281c4cab6b3120b6f78
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-10-21 18:08:48 +0200

    03- vocali

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,9 +5,17 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function vowelNumber(str){
+
+    const vowels= ['a','e','i','o','u']
+
+    return str.toLowerCase().split('').filter(char => vowels.includes(char)).length;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const vowelsInside= vowelNumber(word);
+console.log(vowelsInside);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
