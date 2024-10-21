d52d7376a8bcfd170641c29f9569bb58fdc37681
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-10-21 16:54:34 +0200

    02-filtri

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function namesWith(text){
+    for (let i = 0; 1 < names.length; i++){
+        const initial = names[i];
+        if(initial.includes(text)){
+            console.log(initial)
+        }
+    }
 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
-
+const userLetter = prompt('inserisci una lettera')
+const namesValid = namesWith(userLetter);
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
