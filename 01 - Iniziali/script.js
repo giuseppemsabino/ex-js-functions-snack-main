a0a8fab73b54b4f114d9be19502d55983e096eeb
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-10-21 16:33:14 +0200

    01- inziali

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -4,10 +4,14 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function takeInitial(names){
 
+    return names.map(names => names.charAt(0));
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const initial = takeInitial(names)
+console.log(initial)
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
